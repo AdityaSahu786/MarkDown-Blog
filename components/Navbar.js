788d09175624d56512cb19187f3bd11d3cdaef02
AdityaSahu786 <sahuadi786183@gmail.com>
2024-09-17 18:23:16 +0530

    Added hamburger menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,15 @@
 import React from 'react'
 import Link from 'next/link';
 import { Button } from "@/components/ui/button"
-import { HamIcon } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 
 const Navbar = () => {
@@ -28,16 +36,46 @@ const Navbar = () => {
         </div>
           </div>
           <div className="md:hidden">
-            <button className="text-white focus:outline-none">
+          <Sheet>
+  <SheetTrigger>Open
+  
+
+            
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M412h16m-7 6h7"></path>
               </svg>
-            </button>
+            </SheetTrigger>
+            <SheetContent>
+    <SheetHeader>
+      <SheetTitle className="font-bold my-4">Anakin's Blog</SheetTitle>
+      <SheetDescription>
+      <div className="flex flex-col gap-5">
+          <Link href="/"> Home
+          </Link>
+          <Link href="/about">
+             About
+          </Link>
+          <Link href="/contact">
+              Contact
+          </Link>
+          <div>
+        <Button className="mx-1 text-xs" variant="outline">Login</Button>
+        <Button className="mx-1  text-xs" variant="outline">Signup</Button>
+
+        </div>
+          </div>
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
         </div>
        
         
       </div>
+
+      
+
     </nav>
   );
 };
