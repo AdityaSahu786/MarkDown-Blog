669ba1ba400ca808d11d42522f396e4c9242b17e
AdityaSahu786 <sahuadi786183@gmail.com>
2024-09-26 21:03:33 +0530

    Setting Up Content process setup

--- app/blogpost/[slug]/page.js
+++ app/blogpost/[slug]/page.js
@@ -1,7 +1,15 @@
 import fs from "fs";
 import matter from 'gray-matter';
+import { notFound } from "next/navigation";
+import rehypeDocument from 'rehype-document'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import { unified } from "unified";
 
-export default function Page({ params }) {
+
+export default async function Page({ params }) {
    /* const blog = {
         title: "TypeScript Tutorial",
         author: "John Doe",
@@ -18,19 +26,28 @@ export default function Page({ params }) {
     
         const fileContent = fs.readFileSync(filepath, "utf-8")
         const {content, data} = matter(fileContent) 
+     
+      const processor = unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeDocument, {title: '👋🌍'})
+      .use(rehypeFormat)
+      .use(rehypeStringify)
+
+      const htmlContent = (await processor.process(content)).toString()
+      
     
-   
 
     return (
         <div className="max-w-5xl mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-2">{data.title}</h1>
+            <h1 className="text-4xl font-bold mb-2">{data.title}</h1>
             <p className="text-base mb-2 border-1-4 border-gray-500 pl-4 italic">&quot;{data.description}&quot;</p>
             <div className="flex gap-2">
             <p className="text-gray-500 mb-4 italic">By {data.author}</p>
             <p className="text-gray-500 mb-4">{data.date}</p>
             </div>
             
-            <div dangerouslySetInnerHTML={{ __html: content }} className="prose"></div>
+            <div dangerouslySetInnerHTML={{ __html: htmlContent }} className="prose"></div>
         </div>
     )
 }
