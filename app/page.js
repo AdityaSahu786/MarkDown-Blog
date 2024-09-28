66f848bed1b7b85419fd3fdfcfb10e3103003089
AdityaSahu786 <sahuadi786183@gmail.com>
2024-09-28 12:50:03 +0530

    About Page Revamp

--- app/page.js
+++ app/page.js
@@ -1,13 +1,32 @@
+"use client"
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import Typed from "typed.js";
+import React, {useRef, useEffect} from "react";
 
 export default function Home() {
+
+    // Create reference to store the DOM element containing the animation
+    const el = useRef(null);
+
+    useEffect(() => {
+      const typed = new Typed(el.current, {
+        strings: ['Artificial Intelligence', 'Machine Learning', 'WEB3', 'WebDevelopment'],
+        typeSpeed: 50,
+      });
+  
+      return () => {
+        // Destroy Typed instance during cleanup to stop animation
+        typed.destroy();
+      };
+    }, []);
+
   return (
     <main>
       <section className="container px-4 py-10 mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center">
         <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8">
           <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
-            A <span className="font-semibold">free repository</span> for community <br className="hidden lg:block" /> components using <span className="font-semibold underline decoration-primary">Tailwind CSS</span>
+            A <span className="font-semibold">free repository</span> for community <br className="hidden lg:block" /> components using <span className="font-semibold underline decoration-primary"><span ref={el} /></span>
           </h1>
           <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
             Open source Tailwind UI components and templates to <br className="hidden lg:block" /> bootstrap your new apps, projects or landing sites!
