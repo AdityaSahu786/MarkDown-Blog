a832f6f4505bfe137c6dff10f095616617c094f0
AdityaSahu786 <sahuadi786183@gmail.com>
2024-09-28 13:53:06 +0530

    Added Top Loading Bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-
+"use client"
 import React from 'react'
 import Link from 'next/link';
 import { Button } from "@/components/ui/button"
@@ -11,11 +11,43 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import { ModeToggle } from './theme-btn';
+import LoadingBar from 'react-top-loading-bar';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 
 const Navbar = () => {
+
+  const [progress, setProgress] = useState(0)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    setProgress(30)
+
+    setTimeout(() => {
+       setProgress(70)
+    },100);
+
+    setTimeout(() => {
+        setProgress(100)
+    }, 400);
+  
+  }, [pathname])
+  
+  useEffect(() => {
+    setTimeout(() => {
+        setProgress(0)
+    }, 500);
+  }, [])
+  
+
   return (
     <nav className="p-4 bg-background/50 sticky top-0  backdrop-blur border-b z-10">
+      <LoadingBar
+        color='#f11946'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <div className="container mx-auto flex justify-between items-center">
          <Link href={"/"}><div className=" text-lg font-bold">
             AnakinBlog
